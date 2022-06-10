60bd9f94bb0cf0b895abbe9e0efdf5946235d141
Gage <yaujin@protonmail.com>
2022-06-09 22:52:03 -0500

    made function to create divs for 16x16 grid

--- javascript.js
+++ javascript.js
@@ -1,1 +1,17 @@
 const container = document.querySelector(".container");
+
+function makeGrid(rows, cols) {
+  container.style.setProperty("--grid-rows", rows);
+
+  container.style.setProperty("--grid-cols", cols);
+
+  for (i = 0; i < rows * cols; i++) {
+    const gridBox = document.createElement("div");
+
+    gridBox.innerText = i + 1;
+
+    container.appendChild(gridBox).className = "grid-item";
+  }
+}
+
+makeGrid(16, 16);
